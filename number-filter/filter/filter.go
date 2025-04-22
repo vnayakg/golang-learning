ef07301593dd30a2f1ec5ee17abc74c8b1b62678
Vinayak Gaikwad <gaikwadvinayak291@gmail.com>
2025-04-22 17:33:01 +0530

    Add ability to filter on all predicates

--- number-filter/filter/filter.go
+++ number-filter/filter/filter.go
@@ -1,41 +1,50 @@
 package filter
 
 func filterEvenNumbers(numbers []int) []int {
-	return filterNumbersOnPredicate(numbers, isEven)
+	return filterNumbersOnAllPredicates(numbers, []func(int) bool{isEven})
 }
 
 func filterOddNumbers(numbers []int) []int {
-	return filterNumbersOnPredicate(numbers, isOdd)
+	return filterNumbersOnAllPredicates(numbers, []func(int) bool{isOdd})
 }
 
 func filterPrimeNumbers(numbers []int) []int {
-	return filterNumbersOnPredicate(numbers, isPrime)
+	return filterNumbersOnAllPredicates(numbers, []func(int) bool{isPrime})
 }
 
 func filterOddPrimeNumbers(numbers []int) []int {
-	return filterNumbersOnPredicate(numbers, func(num int) bool { return isOdd(num) && isPrime(num) })
+	return filterNumbersOnAllPredicates(numbers, []func(int) bool{isOdd, isPrime})
 }
 
 func filterEvenAndMultipleOfFiveNumbers(numbers []int) []int {
-	return filterNumbersOnPredicate(numbers, func(num int) bool { return isEven(num) && isMultipleOfFive(num) })
+	return filterNumbersOnAllPredicates(numbers, []func(int) bool{isEven, isMultipleOfFive})
 }
 
 func filterOddAndMultipleOfThreeAndGreaterThanTen(numbers []int) []int {
-	return filterNumbersOnPredicate(numbers, func(num int) bool { return isOdd(num) && isMultipleOfThree(num) && isGreaterThanTen(num) })
+	return filterNumbersOnAllPredicates(numbers, []func(int) bool{isOdd, isMultipleOfThree, isGreaterThanTen})
 }
 
 func isOdd(num int) bool { return num%2 != 0 }
 
 func isEven(num int) bool { return num%2 == 0 }
 
-func filterNumbersOnPredicate(numbers []int, predicate func(int) bool) []int {
+func filterNumbersOnAllPredicates(numbers []int, predicates []func(int) bool) []int {
 	filteredNumbers := make([]int, 0, len(numbers))
 	for _, num := range numbers {
-		if predicate(num) {
+		if isAllApplicable(num, predicates) {
 			filteredNumbers = append(filteredNumbers, num)
 		}
 	}
 	return filteredNumbers
+}
+
+func isAllApplicable(number int, predicates []func(int) bool) bool {
+	for _, predicate := range predicates {
+		if !predicate(number) {
+			return false
+		}
+	}
+	return true
 }
 
 func isPrime(number int) bool {
